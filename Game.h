2713aa89f3c4aacd079029a4a4e9eb38c52d117b
sinhtruong1012 <161494846+sinhtruong1012@users.noreply.github.com>
2024-05-22 22:03:26 +0700

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,13 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "Function.h"
+#include "Player.h"
+#include "Barrier.h"
+#include "Health.h"
+#include <vector>
+#include <algorithm>
+void game(SDL_Renderer* renderer, vector<int> &highscore,int tempNameBG);
+
+
+#endif // GAME_H
