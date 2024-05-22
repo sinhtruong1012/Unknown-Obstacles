d35e890067b6fb06863236f0a35c142f897b5524
sinhtruong1012 <161494846+sinhtruong1012@users.noreply.github.com>
2024-05-22 22:00:01 +0700

    Create Barrier.h

---
+++ Barrier.h
@@ -0,0 +1,29 @@
+#ifndef BARRIER_H
+#define BARRIER_H
+
+#include "Player.h"
+#include "Function.h"
+
+using namespace std;
+
+struct Barrier
+{
+    int x;
+    int y;
+    int posNameBarrier = 0;
+    int posBarrier = 1;
+    int speed1 =8, speed2 = 10;
+    Barrier (){};
+    Barrier (int _x,int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+    void renderBarrier(SDL_Renderer* renderer);
+
+    void move1(int countScore);
+    void move2(int countScore);
+
+    bool checkCollision(Player playerCheck);
+};
+#endif // BARRIER_H
